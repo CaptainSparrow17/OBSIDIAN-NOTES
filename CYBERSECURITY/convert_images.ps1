47ce5c23fdb04227c9ee2686b061638692f52cac
CaptainSparrow17 <atishojha17@gmail.com>
2025-02-18 13:33:44 +0000

    vault backup: 2025-02-18 13:33:44

--- CYBERSECURITY/convert_images.ps1
+++ CYBERSECURITY/convert_images.ps1
@@ -1,5 +1,5 @@
 # Get all .md files in the current directory
 Get-ChildItem -Filter *.md | ForEach-Object {
-    (Get-Content $_.FullName) -replace '\!\[\[Pasted image ', '![](../Attachemnts/Pasted%20image ' `
+    (Get-Content $_.FullName) -replace '\!\[\[Pasted image ', '![](../Attachements/Pasted%20image ' `
                                 -replace '\]\]', ')' | Set-Content $_.FullName
 }
